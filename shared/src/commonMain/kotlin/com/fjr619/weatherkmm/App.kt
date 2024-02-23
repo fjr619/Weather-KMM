147698371fc679acf803f55d1d918c7ff0613fcb
Franky Wijanarko <frank.jr.619@gmail.com>
2024-02-23 18:46:24 +0700

    a12

--- shared/src/commonMain/kotlin/com/fjr619/weatherkmm/App.kt
+++ shared/src/commonMain/kotlin/com/fjr619/weatherkmm/App.kt
@@ -40,8 +40,8 @@ fun App(
         Surface(
             modifier = Modifier.fillMaxSize()
         ) {
-            val a1 = stringResource(MR.strings.add)
-            var greetingText by remember { mutableStateOf("$a1, World!") }
+            val a12 = stringResource(MR.strings.add)
+            var greetingText by remember { mutableStateOf("$a12, World!") }
             var showImage by remember { mutableStateOf(false) }
             Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
                 Button(
